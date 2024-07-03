cf7b17fb7aa3cf0067ac27fe0957b44b7c8f57ae
Vansh Kalra <vanshkalra645@gmail.com>
2024-07-03 22:01:57 +0530

    CRUD on file system

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-const { log } = require('console');
 const fs = require('fs');
 const path = require('path');
-const dirPath = path.join(__dirname, 'files');
-console.warn(dirPath)
-// for(i = 0; i < 5; i++) {
-//     fs.writeFileSync(`${dirPath}/file${i}.txt`, 'a text file')
-// }
+const dirPath = path.join(__dirname,'crud');
+const filePath = `${dirPath}/apple.txt`
 
-fs.readdir(dirPath, (err, files) => {
-    files.forEach((file) => {
-        console.log("file name is: ", file)
-    })
-})
+// CREATE FILE
+// fs.writeFileSync(filePath, 'file content');
+
+//READ FILE
+// fs.readFile(filePath,'utf-8' ,(err, item) => {
+//     console.log(item)
+// })
+
+// UPDATE FILE
+// fs.appendFile(filePath, ' while this is new file content appended', (err) => {
+//     if(!err) console.log("File is updated!"); 
+// })
+
+//RENAME FILE
+// fs.rename(filePath, `${dirPath}/fruit.txt`, (err) => {
+//     if(!err) console.log("File has been renamed!")
+// })
+
+//DELETE FILE
+fs.unlinkSync(`${dirPath}/fruit.txt`)
