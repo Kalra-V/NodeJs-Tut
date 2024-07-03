db0104ec19301a24488a1f38c29894b3e81b066d
Vansh Kalra <vanshkalra645@gmail.com>
2024-07-03 21:49:46 +0530

    timestamp: 2:07:50

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+const { log } = require('console');
 const fs = require('fs');
+const path = require('path');
+const dirPath = path.join(__dirname, 'files');
+console.warn(dirPath)
+// for(i = 0; i < 5; i++) {
+//     fs.writeFileSync(`${dirPath}/file${i}.txt`, 'a text file')
+// }
 
-const input = process.argv
-
-if(input[2] == 'add') {
-    fs.writeFileSync(input[3], input[4])
-} else if (input[2] == 'remove') {
-    fs.unlinkSync(input[3]);
-} else {
-    console.log("invalid input")
-}
+fs.readdir(dirPath, (err, files) => {
+    files.forEach((file) => {
+        console.log("file name is: ", file)
+    })
+})
