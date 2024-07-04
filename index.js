6b6969e9942c710a516b86e02744bf684531484f
Vansh Kalra <vanshkalra645@gmail.com>
2024-07-04 22:38:42 +0530

    Routing with express and html rendering, timestamp: 3:15:00

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require('express')
 const app = express();
 app.get('', (req, res) => {
-    res.send('Hello, from express MF.')
+    res.send(`
+        <h1>Hello, from express MF.</h1>
+        <a href="/about"> Go to about page. </a>
+        `)
 })
 app.get('/about', (req, res) => {
-    res.send('Hello, from about AND express MF.')
+    res.send(`
+        <input type="text" placeholder="Username" value="${req.query.name}"/>
+        <a href="/"> Go to home page. </a>
+        <button>Click me</button>
+        `)
+})
+app.get('/help', (req, res) => {
+    res.send([
+        {
+            name:'vansh',
+            email:'kalra@xyz'
+        },
+        {
+            name:'kalra',
+            email:'kalra@xyz'
+        }
+    ])
 })
 
 app.listen(5000)
