6ef3a75d9ebbe6b22fac489f7224096b24067b8c
Vansh Kalra <vanshkalra645@gmail.com>
2024-07-08 15:52:51 +0530

    Read data from Mongo in Node, timestamp: 5:17:00

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const { MongoClient } = require('mongodb');
-const url = 'mongodb://localhost:27017'
-const database = 'e-comm'
-const client = new MongoClient(url);
+const dbConnect = require('./mongodb')
 
-async function getData() {
-    let result = await client.connect();
-    let db = result.db(database);
-    let collection = db.collection('products');
-    let response = await collection.find({}).toArray()
-    console.log(response)
+
+// dbConnect().then((res) => {
+//     res.find({name:'Vansh Kalra'}).toArray().then((data) => {
+//         console.log(data)
+//     })
+// })
+// console.log(dbConnect())
+
+const main = async () => {
+    let data = await dbConnect();
+    data = await data.find().toArray();
+    console.log(data)
 }
-
-getData();
+main()
