4b8b3e7f17df3ecf60047793cd00f2a0dab1e3f9
Vansh Kalra <vanshkalra645@gmail.com>
2024-07-08 22:22:49 +0530

    Delete data from mongoDB in node

---
+++ delete.js
@@ -0,0 +1,12 @@
+const dbConnect = require("./mongodb");
+
+const deleteData = async () => {
+    let data = await dbConnect()
+    let result = await data.deleteMany({name:'A T'})
+    console.log(result)
+    if(result.acknowledged) {
+        console.log("Record deleted.")
+    }
+}
+
+deleteData()
