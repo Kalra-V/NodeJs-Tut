685feb1c8f79b54301604bcd316bf6a7bb1d3ea0
Vansh Kalra <vanshkalra645@gmail.com>
2024-07-09 22:43:57 +0530

    CRUD Operations with mongoose

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/e-comm");
 
-const main = async () => {
-    await mongoose.connect("mongodb://localhost:27017/e-comm")
-    const ProductSchema = new mongoose.Schema({
-        name: String,
-        age: Number
-    });
-    const ProductModel = mongoose.model('products', ProductSchema);
-    let data = new ProductModel({
-        name:"U T", age: 22
-    });
-    let result = await data.save();
-    console.log(result);
+const ProductSchema = new mongoose.Schema({
+    name: String,
+    "Age": Number,
+    "Net Worth": String,
+  });
+
+const saveInDB = async () => {
+  
+  const Product = mongoose.model("products", ProductSchema);
+  let data = new Product({
+    name: "U T",
+    "Age": 22,
+    "Net Worth": "$400,000",
+  });
+  let result = await data.save();
+  console.log(result);
+};
+
+const updateInDB = async () =>{
+    const Product = mongoose.model("products", ProductSchema);
+    let data = await Product.updateOne(
+        {name:"Shivani Kalra"},
+        {
+            $set:{"Net Worth": "$500,000", "Age": 25}
+        }
+    )
+    console.log(data)
 }
-main();
+
+const deleteInDB = async () => {
+    const Product = mongoose.model("products", ProductSchema);
+    let data = await Product.deleteMany({name:"U T"});
+    console.log(data);
+}
+
+const findInDB = async () => {
+    const Product = mongoose.model("products", ProductSchema);
+    let data = await Product.findOne({name:"Vansh Kalra"});
+    console.log(data);
+}
+findInDB()
