9de30711c0811b79c84aa507813b977092f04659
Vansh Kalra <vanshkalra645@gmail.com>
2024-07-10 22:07:19 +0530

    Events and Event Emitter in NodeJS

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const os = require('os')
-// console.log(os.arch()) FOR checking 32 bit or 64 bit
-// console.log(os.freemem()/(1024*1024*1024))  // RAM KITNI FREE HAI IN THE SYSTEM, IN BYTES
-// console.log(os.totalmem()/(1024*1024*1024)) TOTAL RAM KITNI HAI
-// console.log(os.hostname()) NAME OF DESKTOP
-// console.log(os.platform()) OS PLATFORM - Win32, macOS
-// console.log(os.userInfo()) INFO ABOUT THE USER 
+const express = require("express")
+const EventEmitter = require("events")
+const app = express();
+
+const event = new EventEmitter();
+
+let count = 0;
+event.on("countAPI", ()=> {
+    count++;
+    console.log("Event called. ", count)
+})
+
+app.get("/", async(req, res) => {
+    res.send("Working")
+    event.emit("countAPI")
+})
+app.get("/search", async(req, res) => {
+    res.send("Working")
+    event.emit("countAPI")
+})
+app.get("/update", async(req, res) => {
+    res.send("Working")
+    event.emit("countAPI")
+})
+
+app.listen(5000);
--- product.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ProductSchema = new mongoose.Schema({
-  name: String,
-  Age: Number,
-  Networth: String,
-});
-
-module.exports = mongoose.model("products", ProductSchema);
