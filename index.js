4ddf8aeb53bb44eeedf29a6cc91568031b6408f0
Vansh Kalra <vanshkalra645@gmail.com>
2024-07-11 21:38:54 +0530

    POST API with MySQL and NodeJS

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const conn = require("./config")
 
 const app = express();
+app.use(express.json())
 
 app.get("/", (req,res) => {
     conn.query("select * from user", (err,result) => {
@@ -10,6 +11,14 @@ app.get("/", (req,res) => {
         } else {
             res.send(result)
         }
+    })
+})
+
+app.post("/", (req,res) => {
+    const data = req.body;
+    conn.query("insert into user set ?", data, (err,result,fields) =>{
+        if(err) err;
+        res.send(result)
     })
 })
 
