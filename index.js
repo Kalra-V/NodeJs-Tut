505e1b6d4c50087484601e3d581b415d1818f775
Vansh Kalra <vanshkalra645@gmail.com>
2024-07-11 21:23:41 +0530

    MySQL connection with NodeJS

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-const express = require("express")
-const EventEmitter = require("events")
-const app = express();
+const mysql = require('mysql')
 
-const event = new EventEmitter();
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user:'root',
+    password:'',
+    database:'test'
+});
+//CHECK CONNECTION
+// conn.connect((err) => {
+//     if(err) {
+//         console.log("Error!")
+//     } else {
+//         console.log("Connected!")
+//     }
+// })
 
-let count = 0;
-event.on("countAPI", ()=> {
-    count++;
-    console.log("Event called. ", count)
+conn.query("select * from user", (err, result) => {
+    console.log("Result: ", result)
 })
-
-app.get("/", async(req, res) => {
-    res.send("Working")
-    event.emit("countAPI")
-})
-app.get("/search", async(req, res) => {
-    res.send("Working")
-    event.emit("countAPI")
-})
-app.get("/update", async(req, res) => {
-    res.send("Working")
-    event.emit("countAPI")
-})
-
-app.listen(5000);
